55f646d0d263145d95cc7d2c5a8a9a4350144416
jrclinicsystem <jrclinic.system@gmail.com>
2026-08-30 00:45:34 -0300

    Pin JR Clinic to primary Supabase project

---
+++ src/integrations/supabase/project.ts
@@ -0,0 +1,3 @@
+export const JR_CLINIC_SUPABASE_PROJECT_ID = "ldrqcbqaekmohwpneekx";
+export const JR_CLINIC_SUPABASE_URL = "https://ldrqcbqaekmohwpneekx.supabase.co";
+export const JR_CLINIC_SUPABASE_PUBLISHABLE_KEY = "sb_publishable_BN_6-1kyzdiaMefWLudU8g_b2yKQa0n";
